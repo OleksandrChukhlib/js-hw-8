fab17a53196cfc8bfc5a306584909e9365147c29
Oleksandr Chukhlib <Chukhlib.aleksandr@gmail.com>
2022-11-21 13:51:46 +0200

    refactored to account for the library SimpleLightBox being installed via npm

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const gallery = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", gallery);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250});
+
+function createGalleryMarkup (galleryItems) {
+	return galleryItems.map(({ preview, original, description }) => {
+		return `
+		<div class="gallery__item">
+		<a class="gallery__item" href="${original}">
+  <img 
+	class="gallery__image" 
+	src="${preview}" 
+	alt="${description}" />
+</a>
+		</a>
+	</div>
+	`
+	}).join("");
+
+}
+
+function onGalleryContainerClick (e) {
+	e.preventDefault();
+	const isGalleryImageEl = e.target.classList.contains("gallery__image");
+	if(!isGalleryImageEl) {
+			return;
+	} 
+
+}
+
